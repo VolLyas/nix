e3c77841c37bd172fb4d2554f408c9096880e0f4
VolLyas <vova.lyashik@gmail.com>
2024-06-28 09:06:58 +0300

    update kitty conf

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -1,5 +1,5 @@
 { config, ... }:
 {
   home.file.".config/kitty/kitty.conf".source = ./kitty.conf;
-  home.file.".config/kitty/current-theme.conf".source = ./currnet-theme.conf;
+  home.file.".config/kitty/current-theme.conf".source = ./current-theme.conf;
 }
